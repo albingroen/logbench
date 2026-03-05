7c863a48a8fb9f9f6a77997afc912175c437d167
Albin Groen <albin.groen@gmail.com>
2026-03-05 08:37:14 +0100

    fix: add missing role to table row

--- src/components/logs/data-table.tsx
+++ src/components/logs/data-table.tsx
@@ -65,6 +65,7 @@ export function DataTable<TData, TValue>({
       <TableBody className="logs">
         {table.getRowModel().rows.map((row) => (
           <TableRow
+            role="button"
             key={row.id}
             data-state={row.getIsSelected() ? 'selected' : undefined}
             data-active={location.pathname.includes(row.original.id)}
