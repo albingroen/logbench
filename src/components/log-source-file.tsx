af16aca8219b46ce44ecf4f7d7e477b27f8ec225
Albin Groen <albin.groen@gmail.com>
2026-03-19 00:47:46 +0100

    fix: line height fix

--- src/components/log-source-file.tsx
+++ src/components/log-source-file.tsx
@@ -11,13 +11,13 @@ export function LogSourceFile({ sourceFile, lineCol }: LogSourceFileProps) {
   const { baseName, dirName, extension } = getSourceFileDetails(sourceFile)
 
   return (
-    <div className="flex items-start gap-1.5">
+    <div className="flex items-start gap-1">
       <FileIcon
         extension={extension}
-        className="size-3.5 text-dim-foreground"
+        className="size-3.5 text-dim-foreground translate-y-0.5"
       />
       <div className="flex flex-col gap-1.5 flex-1">
-        <span className="text-sm leading-none break-all line-clamp-1">
+        <span className="text-sm leading-tight break-all line-clamp-1">
           {baseName}
           {lineCol ? `:${lineCol}` : ''}
         </span>
